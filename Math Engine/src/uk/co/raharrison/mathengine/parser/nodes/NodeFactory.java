ef1cf94d76f15464ca735f59bcaac02cf2c63fd6
Ryan Harrison <mail@raharrison.co.uk>
2012-08-06 19:48:12 +0000

    Improved NodeFactory.createNodeNumberFrom method.

--- Math Engine/src/uk/co/raharrison/mathengine/parser/nodes/NodeFactory.java
+++ Math Engine/src/uk/co/raharrison/mathengine/parser/nodes/NodeFactory.java
@@ -3,18 +3,19 @@ package uk.co.raharrison.mathengine.parser.nodes;
 public class NodeFactory
 {
 	private static final int maxInt = Integer.MAX_VALUE;
-	private static final int maxLength = Integer.toString(maxInt).length();
 
 	public static NodeNumber createNodeNumberFrom(double value)
 	{
-		// TODO : Fix
 		double absValue = Math.abs(value);
-
+		
+		// Greater than max possible number
 		if (absValue > maxInt)
 			return new NodeDouble(value);
+		// Too small
 		else if (absValue < 1.0 / maxInt)
 			return new NodeDouble(value);
-		else if (Double.toString(absValue).split("\\.")[1].length() > maxLength)
+		// Too much precision
+		else if (Double.toString(absValue).split("\\.")[1].length() > 4)
 			return new NodeDouble(value);
 		else
 			return new NodeRational(value);
