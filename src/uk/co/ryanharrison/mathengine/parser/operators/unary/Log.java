b999564ce6ee4795aafda68c0243b90047450d6d
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-06 17:03:31 +0100

    Converted Log operator to a VectorOperator
    The operator now treats all input as a vector through the superclass. If
    the input is a matrix it is converted to a vector of vectors. If the size
    of the internal vectors is two then the log base [0] of [1] will be
    calculated

--- src/uk/co/ryanharrison/mathengine/parser/operators/unary/Log.java
+++ src/uk/co/ryanharrison/mathengine/parser/operators/unary/Log.java
@@ -1,16 +1,38 @@
 package uk.co.ryanharrison.mathengine.parser.operators.unary;
 
-import uk.co.ryanharrison.mathengine.linearalgebra.Matrix;
+import uk.co.ryanharrison.mathengine.parser.nodes.Node;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeConstant;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeDouble;
-import uk.co.ryanharrison.mathengine.parser.nodes.NodeMatrix;
-import uk.co.ryanharrison.mathengine.parser.nodes.NodeNumber;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeVector;
-import uk.co.ryanharrison.mathengine.parser.operators.Determinable;
-import uk.co.ryanharrison.mathengine.parser.operators.UnaryOperator;
 
-public class Log extends UnaryOperator
+public class Log extends VectorOperator
 {
+	@Override
+	protected NodeConstant calculateResultFromVector(NodeVector arg1)
+	{
+		Node[] values = arg1.getValues();
+		NodeConstant[] results = new NodeConstant[arg1.getSize()];
+		for (int i = 0; i < arg1.getSize(); i++)
+		{
+			NodeVector vector = values[i].getTransformer().toNodeVector();
+			if(vector.getSize() == 1)
+			{
+				results[i] = new NodeDouble(Math.log10(vector.getTransformer().toNodeNumber().doubleValue()));
+			}
+			else if(vector.getSize() == 2)
+			{
+				double argument = vector.getValues()[0].getTransformer().toNodeNumber().doubleValue();
+				double base = vector.getValues()[1].getTransformer().toNodeNumber().doubleValue();
+				results[i] = new NodeDouble(Math.log10(base) / Math.log10(argument));
+			}
+			else
+			{
+				throw new IllegalArgumentException("Matrix must have two columns {base, value}");
+			}
+		}
+		return new NodeVector(results);
+	}
+	
 	@Override
 	public String[] getAliases()
 	{
@@ -27,50 +49,6 @@ public class Log extends UnaryOperator
 	public String toLongString()
 	{
 		return "logarithm";
-	}
-
-	@Override
-	public NodeConstant toResult(NodeConstant arg1)
-	{
-		if (arg1 instanceof NodeNumber)
-		{
-			return new NodeDouble(Math.log10(arg1.doubleValue()));
-		}
-		else if (arg1 instanceof NodeVector)
-		{
-			return arg1.applyDeterminable(new Determinable()
-			{
-				@Override
-				public NodeNumber getResult(NodeConstant number)
-				{
-					return new NodeDouble(Math.log10(number.doubleValue()));
-				}
-			});
-		}
-		else if (arg1 instanceof NodeMatrix)
-		{
-			Matrix m = ((NodeMatrix) arg1).toDoubleMatrix();
-
-			if (m.getColumnCount() == 2)
-			{
-				NodeDouble[] results = new NodeDouble[m.getRowCount()];
-
-				for (int i = 0; i < results.length; i++)
-				{
-					results[i] = new NodeDouble(Math.log10(m.get(i, 1)) / Math.log10(m.get(i, 0)));
-				}
-
-				return new NodeVector(results);
-			}
-			else
-			{
-				throw new IllegalArgumentException("Matrix must have two columns");
-			}
-		}
-		else
-		{
-			throw new UnsupportedOperationException("Unsupported arguments to operator 'log'");
-		}
 	}
 
 	@Override
