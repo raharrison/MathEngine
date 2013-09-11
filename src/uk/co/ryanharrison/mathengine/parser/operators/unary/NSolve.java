545a273007c938a48455376603cdd6e3b395e1a3
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-11 21:55:52 +0100

    Converted NSolve operator to a VectorOperator

--- src/uk/co/ryanharrison/mathengine/parser/operators/unary/NSolve.java
+++ src/uk/co/ryanharrison/mathengine/parser/operators/unary/NSolve.java
@@ -1,60 +1,47 @@
 package uk.co.ryanharrison.mathengine.parser.operators.unary;
 
-import uk.co.ryanharrison.mathengine.Function;
 import uk.co.ryanharrison.mathengine.parser.nodes.Node;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeConstant;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeDouble;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeFunction;
 import uk.co.ryanharrison.mathengine.parser.nodes.NodeVector;
-import uk.co.ryanharrison.mathengine.parser.operators.UnaryOperator;
 import uk.co.ryanharrison.mathengine.solvers.ConvergenceCriteria;
 import uk.co.ryanharrison.mathengine.solvers.NewtonRaphsonSolver;
 
-public class NSolve extends UnaryOperator
+public class NSolve extends VectorOperator
 {
 	@Override
-	public NodeConstant toResult(NodeConstant arg1)
+	protected NodeConstant calculateResultFromVector(NodeVector arg1)
 	{
-		if (arg1 instanceof NodeFunction)
+		Node[] elements = arg1.getValues();
+		
+		if(!(elements[0] instanceof NodeFunction))
+			throw new IllegalArgumentException("First argument must be a function");
+		
+		NewtonRaphsonSolver solver = new NewtonRaphsonSolver(((NodeFunction) elements[0]).toFunction());
+		solver.setIterations(25);
+		solver.setConvergenceCriteria(ConvergenceCriteria.NumberOfIterations);
+		
+		if (arg1.getSize() == 2)
 		{
-			Function func = ((NodeFunction) arg1).toFunction();
-			NewtonRaphsonSolver solver = new NewtonRaphsonSolver(func);
-			solver.setIterations(25);
-			solver.setConvergenceCriteria(ConvergenceCriteria.NumberOfIterations);
+			solver.setInitialGuess((int) elements[1].getTransformer().toNodeNumber().doubleValue());
 			return new NodeDouble(solver.solve());
 		}
-		else if (arg1 instanceof NodeVector)
-		{
-			NodeVector vec = (NodeVector) arg1;
-			Node[] elements = vec.getValues();
-			if (vec.getSize() == 1)
-			{
-				if (elements[0] instanceof NodeFunction)
-				{
-					NewtonRaphsonSolver solver = new NewtonRaphsonSolver(((NodeFunction) elements[0]).toFunction());
-					solver.setIterations(25);
-					solver.setConvergenceCriteria(ConvergenceCriteria.NumberOfIterations);
-					return new NodeDouble(solver.solve());
-				}
-				throw new IllegalArgumentException("Must have function in arguments");
-			}
-			else if (vec.getSize() == 2)
-			{
-				if (elements[0] instanceof NodeFunction)
-				{
-					NewtonRaphsonSolver solver = new NewtonRaphsonSolver(((NodeFunction) elements[0]).toFunction());
-					solver.setIterations(25);
-					solver.setInitialGuess((int) elements[1].getTransformer().toNodeNumber().doubleValue());
-					solver.setConvergenceCriteria(ConvergenceCriteria.NumberOfIterations);
-					return new NodeDouble(solver.solve());
-				}
-				throw new IllegalArgumentException("Must have function in arguments");
-			}
-			else
-				throw new IllegalArgumentException("Must have one or two arguments");
-		}
+		
+		return new NodeDouble(solver.solve());
+	}
 
-		throw new IllegalArgumentException("Operator must have a function argument");
+	@Override
+	protected String getExpectedArgumentsString()
+	{
+		return "function, initialGuess";
+	}
+
+	@Override
+	protected void fillAcceptedArguments()
+	{
+		acceptedArgumentLengths.add(1);
+		acceptedArgumentLengths.add(2);
 	}
 
 	@Override
