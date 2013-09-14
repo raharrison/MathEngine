606591f6f4c0d99e965aec2184bc01a886686033
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-14 15:02:16 +0100

    Added documentation to RegressionModel
    Removed coefficient of correlation and determination in the class but
    not yet in all subclasses thus this commit contains build errors.
    
    These methods should be re-added in a future commit

--- src/uk/co/ryanharrison/mathengine/regression/RegressionModel.java
+++ src/uk/co/ryanharrison/mathengine/regression/RegressionModel.java
@@ -1,80 +1,77 @@
 package uk.co.ryanharrison.mathengine.regression;
 
-import uk.co.ryanharrison.mathengine.StatUtils;
-
+/**
+ * Represents a regression model with one defined independent variable. Provides
+ * operations to compute the regression coefficients and evaluate the resulting
+ * function at a certain point.
+ * 
+ */
 public abstract class RegressionModel
 {
-	protected double[] x;
+	/** The X values of the data set points */
+	protected double[] xValues;
 
-	protected double[] y;
+	/** The X values of the data set points */
+	protected double[] yValues;
 
+	/** Have the unknown parameters been calculated yet? */
+	protected boolean computed;
+
+	/**
+	 * Construct a new RegressionModel object with the specified data points
+	 * 
+	 * @param x
+	 *            The X data points
+	 * @param y
+	 *            The Y data points
+	 */
 	public RegressionModel(double[] x, double[] y)
 	{
-		this.x = x;
-		this.y = y;
+		this.xValues = x;
+		this.yValues = y;
+		computed = false;
 	}
 
-	public abstract void compute();
-
-	public abstract double evaluateAt(double x);
-
-	public double getCoefficientOfDetermination()
+	/**
+	 * Get the X data points
+	 * 
+	 * @return The X data points
+	 */
+	public double[] getXValues()
 	{
-		double yMean = StatUtils.mean(y);
-
-		double sse = 0;
-		double sstot = 0;
-
-		for (int i = 0; i < y.length; i++)
-		{
-			sse += Math.pow(y[i] - evaluateAt(x[i]), 2);
-
-			sstot += Math.pow(y[i] - yMean, 2);
-		}
-
-		return 1 - sse / sstot;
+		return this.xValues;
 	}
 
-	public abstract double getCoefficientOfDetermination(BiasType biasType);
+	/**
+	 * Get the Y data points
+	 * 
+	 * @return The Y data points
+	 */
+	public double[] getYValues()
+	{
+		return this.yValues;
+	}
 
+	/**
+	 * Get the computed coefficients from the model that best fit the data set
+	 * 
+	 * @return The computed coefficients or parameters that have been fitted to
+	 *         the data set
+	 */
 	public abstract double[] getCoefficients();
 
-	public double getCorrelationCoefficent()
-	{
-		return Math.sqrt(getCoefficientOfDetermination());
-	}
+	/**
+	 * Find the best fit values of the unknown parameters in this regression
+	 * model
+	 */
+	public abstract void compute();
 
-	public abstract double getCorrelationCoefficient(BiasType biasType);
-
-	public double[] getResiduals()
-	{
-		double[] results = new double[y.length];
-
-		for (int i = 0; i < y.length; i++)
-		{
-			results[i] = y[i] - evaluateAt(x[i]);
-		}
-
-		return results;
-	}
-
-	public double[] getX()
-	{
-		return x;
-	}
-
-	public double[] getY()
-	{
-		return y;
-	}
-
-	public void setX(double[] x)
-	{
-		this.x = x;
-	}
-
-	public void setY(double[] y)
-	{
-		this.y = y;
-	}
+	/**
+	 * Evaluate the computed model at a point x
+	 * 
+	 * @param x
+	 *            The point to evaluate the model at
+	 * @return The underlying models computed function evaluated at x
+	 */
+	public abstract double evaluateAt(double x);
 }
