14977a0f5e6c0e87dc58934834ee4bf9d6ed1154
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-22 16:29:15 +0100

    Added documentation to Error function

--- src/uk/co/ryanharrison/mathengine/special/Erf.java
+++ src/uk/co/ryanharrison/mathengine/special/Erf.java
@@ -1,12 +1,31 @@
 package uk.co.ryanharrison.mathengine.special;
 
+/**
+ * Class representing the Error function
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public final class Erf
 {
 
+	/**
+	 * Not permitted to instantiate an object of this class
+	 */
 	private Erf()
-	{}
+	{
+	}
 
-	// From http://www.johndcook.com/csharp_erf.html
+	// From
+	/**
+	 * Calculate the value of the Error function at x
+	 * 
+	 * @see <a
+	 *      href="http://www.johndcook.com/csharp_erf.html">http://www.johndcook.com/csharp_erf.html</a>
+	 * @param x
+	 *            The parameter to the error function
+	 * @return The result of the calculation of Error(x)
+	 */
 	public static double erf(double x)
 	{
 		// constants
@@ -25,12 +44,18 @@ public final class Erf
 
 		// A&S formula 7.1.26
 		double t = 1.0 / (1.0 + p * x);
-		double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t
-				* Math.exp(-x * x);
+		double y = 1.0 - (((((a5 * t + a4) * t) + a3) * t + a2) * t + a1) * t * Math.exp(-x * x);
 
 		return sign * y;
 	}
 
+	/**
+	 * Calculate the value of the complementary Error function at x
+	 * 
+	 * The complementary error function is defined as 1 - {@link #erf(double)}(x)
+	 * @param x The parameter to the complementary error function
+	 * @return The result of the calculation erfc(x)
+	 */
 	public static double erfc(double x)
 	{
 		return 1 - erf(x);
