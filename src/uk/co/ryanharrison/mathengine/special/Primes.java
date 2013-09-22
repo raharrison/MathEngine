191f1a0a6a3ea9f70a53bc7e570a55f18ca48bc4
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-22 16:56:05 +0100

    Added documentation to Primes class

--- src/uk/co/ryanharrison/mathengine/special/Primes.java
+++ src/uk/co/ryanharrison/mathengine/special/Primes.java
@@ -4,25 +4,53 @@ import java.util.ArrayList;
 import java.util.LinkedHashSet;
 import java.util.List;
 
+/**
+ * Class representing the infinite sequence of prime numbers
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public final class Primes
 {
+	/**
+	 * Get a List of the distinct prime factors of a number.
+	 * 
+	 * That is a list of the prime factors of a number that are all unique
+	 * 
+	 * @param n
+	 *            The number to use
+	 * @return A List of the distinct prime factors of n
+	 */
 	public static List<Long> distinctPrimeFactors(long n)
 	{
+		// generate the prime factors
 		List<Long> factors = primeFactors(n);
 
+		// add the factors to a set which maintains order. This will remove all
+		// duplicates
 		LinkedHashSet<Long> distinct = new LinkedHashSet<>(factors);
 
+		// return the unique factors as a list
 		return new ArrayList<Long>(distinct);
 	}
 
+	/**
+	 * Determines whether or not a number is prime
+	 * 
+	 * @param n
+	 *            The number to test
+	 * @return True if the number n is prime, otherwise false
+	 */
 	public static boolean isPrime(long n)
 	{
 		n = Math.abs(n);
 
 		boolean prime = true;
 
+		// go through each odd number up to the square root of n
 		for (long i = 3; i <= Math.sqrt(n); i += 2)
 		{
+			// if the number is a factor, then the number is not prime
 			if (n % i == 0)
 			{
 				prime = false;
@@ -30,6 +58,7 @@ public final class Primes
 			}
 		}
 
+		// account for the special cases and return a result
 		if ((n % 2 != 0 && prime && n > 2) || n == 2)
 		{
 			return true;
@@ -40,10 +69,20 @@ public final class Primes
 		}
 	}
 
+	/**
+	 * Get the next prime number in the sequence of primes after the specified
+	 * number
+	 * 
+	 * @param n
+	 *            The number to generate the next prime after
+	 * @return The next prime number after n
+	 */
 	public static long nextPrime(long n)
 	{
+		// we want the next prime number
 		n++;
 
+		// make sure the number is not even
 		if (n % 2 == 0)
 		{
 			n++;
@@ -51,6 +90,7 @@ public final class Primes
 
 		long result = n;
 
+		// while the number is not prime move to the next odd number
 		while (!isPrime(result))
 		{
 			result += 2;
@@ -59,10 +99,20 @@ public final class Primes
 		return result;
 	}
 
+	/**
+	 * Get the previous prime number in the sequence of primes before the
+	 * specified number
+	 * 
+	 * @param n
+	 *            The number to generate the next prime before
+	 * @return The previous prime number before n
+	 */
 	public static long previousPrime(long n)
 	{
+		// we want the prime number before n
 		n--;
 
+		// make sure the number is not even
 		if (n % 2 == 0)
 		{
 			n--;
@@ -70,6 +120,7 @@ public final class Primes
 
 		long result = n;
 
+		// while the number is not prime move to the previous odd number
 		while (!isPrime(result))
 		{
 			result -= 2;
@@ -78,17 +129,26 @@ public final class Primes
 		return result;
 	}
 
+	/**
+	 * Generate a List of the prime factors of a number
+	 * 
+	 * @param n
+	 *            The number to generate the prime factors of
+	 * @return a List of the prime factors of the number n
+	 */
 	public static List<Long> primeFactors(long n)
 	{
 		long num = Math.abs(n);
 
 		List<Long> factors = new ArrayList<Long>();
 
+		// account for special case
 		if (num == 1)
 		{
 			factors.add(num);
 		}
 
+		// generate the factors
 		for (long i = 2; i <= num / i; i++)
 		{
 			while (num % i == 0)
@@ -103,7 +163,7 @@ public final class Primes
 			factors.add(num);
 		}
 
-		// negate first factors if necessary
+		// negate first factors if necessary (to make them all positive)
 		if (n < 0 && factors.size() != 0)
 		{
 			long first = factors.get(0);
