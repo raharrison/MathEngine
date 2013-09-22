303338a7c7bbc67087a70c7942bea2a151af8f5c
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-22 21:24:31 +0100

    Added documentation

--- src/uk/co/ryanharrison/mathengine/integral/RectanglePosition.java
+++ src/uk/co/ryanharrison/mathengine/integral/RectanglePosition.java
@@ -1,5 +1,14 @@
 package uk.co.ryanharrison.mathengine.integral;
 
+/**
+ * Enumeration representing the position of rectangle within the integral.
+ * 
+ * For use in the RectangularIntegrator which uses rectangles to estimate the
+ * integral of a function
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public enum RectanglePosition
 {
 	Left, Midpoint, Right;
--- src/uk/co/ryanharrison/mathengine/integral/RectangularIntegrator.java
+++ src/uk/co/ryanharrison/mathengine/integral/RectangularIntegrator.java
@@ -2,21 +2,48 @@ package uk.co.ryanharrison.mathengine.integral;
 
 import uk.co.ryanharrison.mathengine.Function;
 
+/**
+ * Class representing an {@link IntegrationMethod} that uses rectangles to
+ * estimate the integral
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public class RectangularIntegrator extends IntegrationMethod
 {
+	/** The position of the rectangles used to estimate the integral */
 	private RectanglePosition position;
 
+	/**
+	 * Construct a new {@link RectangularIntegrator} with the specified target
+	 * {@link Function}
+	 * 
+	 * @param function
+	 *            The function to estimate the integral for
+	 */
 	public RectangularIntegrator(Function function)
 	{
 		this(function, RectanglePosition.Midpoint);
 	}
 
+	/**
+	 * Construct a new {@link RectangularIntegrator} with the specified target
+	 * {@link Function} and {@link RectanglePosition}
+	 * 
+	 * @param function
+	 *            The function to estimate the integral for
+	 * @param position
+	 *            The position of the rectangles used to estimate the integral
+	 */
 	public RectangularIntegrator(Function function, RectanglePosition position)
 	{
 		super(function);
 		this.position = position;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public double integrate()
 	{
