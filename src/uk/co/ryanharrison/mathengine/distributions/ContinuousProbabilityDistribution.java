7f65dbddf97e1cc9c6f78224469cf51dc29ec6f4
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-23 19:26:42 +0100

    Added documentation to probability distribution base classes

--- src/uk/co/ryanharrison/mathengine/distributions/ContinuousProbabilityDistribution.java
+++ src/uk/co/ryanharrison/mathengine/distributions/ContinuousProbabilityDistribution.java
@@ -1,11 +1,46 @@
 package uk.co.ryanharrison.mathengine.distributions;
 
-public abstract class ContinuousProbabilityDistribution extends
-		ProbabilityDistribution
+/**
+ * Class representing a {@link ProbabilityDistribution} that can be applied to
+ * continuous random variables
+ * 
+ * @author Ryan Harrison
+ * 
+ */
+public abstract class ContinuousProbabilityDistribution extends ProbabilityDistribution
 {
+	/**
+	 * Calculate the area of the distribution function from negative infinity to
+	 * x
+	 * 
+	 * @param x
+	 *            The upper bound of the integral calculation
+	 * @return The area of the distribution function from negative infinity to x
+	 */
 	public abstract double cumulative(double x);
 
+	/**
+	 * Calculate the value of the distribution function at x
+	 * 
+	 * @param x
+	 *            The value to evaluate at
+	 * @return The value of the distribution function at x
+	 */
 	public abstract double density(double x);
 
+	/**
+	 * Find the value of the upper bound x which forms an integral calculation
+	 * with negative infinity that results in a probability value of p
+	 * 
+	 * This is the inverse of the cumulative function above.
+	 * 
+	 * For example if x = 3.5 and cumulative of 3.5 is 0.7 = p, then inverse
+	 * cumulative of 0.7 (p) = 3.5 (the original x value)
+	 * 
+	 * @param p
+	 *            The probability area
+	 * @return The upper bound of the integral calculation which results in a
+	 *         probability of p
+	 */
 	public abstract double inverseCumulative(double p);
 }
--- src/uk/co/ryanharrison/mathengine/distributions/ProbabilityDistribution.java
+++ src/uk/co/ryanharrison/mathengine/distributions/ProbabilityDistribution.java
@@ -1,4 +1,11 @@
 package uk.co.ryanharrison.mathengine.distributions;
 
+/**
+ * Base class representing a probability distribution
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public abstract class ProbabilityDistribution
-{}
+{
+}
