1cf51536241d7f79d52fdb5ed5787e9c00239e87
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-24 15:27:49 +0100

    Added documentation

--- src/uk/co/ryanharrison/mathengine/gui/MainFrame.java
+++ src/uk/co/ryanharrison/mathengine/gui/MainFrame.java
@@ -15,35 +15,66 @@ import uk.co.ryanharrison.mathengine.Utils;
 import uk.co.ryanharrison.mathengine.parser.AngleUnit;
 import uk.co.ryanharrison.mathengine.parser.Evaluator;
 
+/**
+ * Frame window to act as a visual interface into the {@link Evaluator} class.
+ * 
+ * Users can insert an expression and upon the enter key being pressed, the
+ * expression will be evaluated and displayed in the output field
+ * 
+ * @author Ryan Harrison
+ * 
+ */
 public class MainFrame extends JFrame
 {
+	/** Serialization identifier */
 	private static final long serialVersionUID = -3046696925938478655L;
 
+	/**
+	 * Main entry point of the application
+	 * 
+	 * @param args
+	 *            Command line parameters
+	 */
 	public static void main(String[] args)
 	{
+		// Run and display the frame on the event dispatch thread
 		SwingUtilities.invokeLater(new Runnable()
 		{
 			@Override
 			public void run()
 			{
 				MainFrame frame = new MainFrame();
+				// Show in the center of the screen
 				frame.setLocationRelativeTo(null);
 				frame.setVisible(true);
 			}
 		});
 	}
 
+	/** Attributes to give a specific colour to the output */
 	private SimpleAttributeSet bold, red;
+
+	/** Panel to house the input and output areas */
 	private JPanel panel;
+
+	/** TextPane to show the results of the expression evaluations */
 	private JTextPane output;
+
+	/** Input area where the user can type in an expression */
 	private JTextArea input;
 
+	/** Evaluator to evaluate the expressions given by the user */
 	private Evaluator evaluator;
 
+	/**
+	 * Construct a new frame, setting up the components and adding event
+	 * handlers
+	 */
 	public MainFrame()
 	{
 		super("Expression evaluator");
 
+		// Initialise fields
 		bold = new SimpleAttributeSet();
 		StyleConstants.setBold(bold, true);
 
@@ -55,6 +86,7 @@ public class MainFrame extends JFrame
 
 		panel = new JPanel();
 
+		// Construct the layout of the frame
 		BorderLayout layout = new BorderLayout(25, 25);
 		panel.setLayout(layout);
 
@@ -70,75 +102,114 @@ public class MainFrame extends JFrame
 		JScrollPane pane2 = new JScrollPane(input);
 		input.addKeyListener(new KeyListener()
 		{
+			/**
+			 * Handle the key pressed event of the input field
+			 * 
+			 * If the enter key is pressed handle evaluate the input and display
+			 * the message
+			 * 
+			 * If 'clear' is inserted, clear the output area of all text
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void keyPressed(KeyEvent event)
 			{
+				// We are only concerned when the user presses the enter key
 				if (!(event.getKeyCode() == KeyEvent.VK_ENTER))
 					return;
 
 				String expression = input.getText().trim();
 				if (!expression.equals(""))
 				{
+					// If the input is clear, clear the output area of all text
 					if (expression.equalsIgnoreCase("clear"))
 					{
 						output.setText("");
 					}
+					// Otherwise evaluate the input and display the results
 					else
 					{
 						evaluateAndDisplayResult(expression);
 					}
 				}
 
+				// Clear the input text to get ready for new input from the user
 				event.consume();
 				input.setText("");
 			}
 
+			/**
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void keyReleased(KeyEvent arg0)
-			{}
+			{
+			}
 
+			/**
+			 * {@inheritDoc}
+			 */
 			@Override
 			public void keyTyped(KeyEvent arg0)
-			{}
+			{
+			}
 		});
 
 		panel.add(pane2, BorderLayout.SOUTH);
 
+		// Add the panel to the frame and set the frame parameters
 		add(panel);
 		pack();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(500, 500);
 		setResizable(false);
 
+		// Focus the input area
 		input.requestFocus();
 	}
 
+	/**
+	 * Evaluates the given expression through the {@link Evaluator} object and
+	 * displays the result in the output {@link TextPane}
+	 * 
+	 * If the expression was successfully evaluated, display the result in
+	 * black, if there was an error during evaluation, display the error message
+	 * in red
+	 * 
+	 * @param expression
+	 *            The expression to evaluate
+	 */
 	private void evaluateAndDisplayResult(String expression)
 	{
 		try
 		{
-
+			// Compile the expression into an expression tree
 			evaluator.compileTree(expression);
+
+			// Construct a message containing the input which appears before the
+			// output
 			String inputTreeMessage = "> "
-					+ Utils.removeOuterParenthesis(evaluator
-							.getCachedTreeToString()) + "\n";
+					+ Utils.removeOuterParenthesis(evaluator.getCachedTreeToString()) + "\n";
 
-			output.getDocument().insertString(output.getDocument().getLength(),
-					inputTreeMessage, null);
+			// Add the input to the end of the output area
+			output.getDocument().insertString(output.getDocument().getLength(), inputTreeMessage,
+					null);
 
-			String outputTreeMessage = evaluator.evaluateCachedTreeString()
-					+ "\n";
+			// Construct the output message
+			String outputTreeMessage = evaluator.evaluateCachedTreeString() + "\n";
 
-			output.getDocument().insertString(output.getDocument().getLength(),
-					outputTreeMessage, bold);
+			// Add the output message to the end of the output area in bold
+			output.getDocument().insertString(output.getDocument().getLength(), outputTreeMessage,
+					bold);
 		}
 		catch (Exception e)
 		{
 			try
 			{
-				output.getDocument().insertString(
-						output.getDocument().getLength(), e.toString() + "\n",
-						red);
+				// If there was an error during evaluation, add the error
+				// message to the end of the output area in red
+				output.getDocument().insertString(output.getDocument().getLength(),
+						e.toString() + "\n", red);
 			}
 			catch (BadLocationException e1)
 			{
