4b24fcd775d2d8b31ea25df3645e86df18883190
Ryan Harrison <mail@ryanharrison.co.uk>
2013-09-25 15:37:47 +0100

    Added DifferentiationMethod enumeration
    This represents the different methods in which the values of the derivative
    of the target function can be obtained when using specific solvers.

---
+++ src/uk/co/ryanharrison/mathengine/solvers/DifferentiationMethod.java
@@ -0,0 +1,13 @@
+package uk.co.ryanharrison.mathengine.solvers;
+
+/**
+ * Enumeration representing different methods of retrieving values of the
+ * derivative of a target function
+ * 
+ * @author Ryan Harrison
+ * 
+ */
+public enum DifferentiationMethod
+{
+	Numerical, Symbolic, Predefined
+}
